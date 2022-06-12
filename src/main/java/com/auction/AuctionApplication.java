2566d748ccf93bfd7eeb6a35daf75eef5ac37fe8
leonardevans <lemutugi@gmail.com>
2022-06-12 05:03:51 -0400

    configired spring application builder by extending SpringBootServletInitializer in the main class

--- src/main/java/com/auction/AuctionApplication.java
+++ src/main/java/com/auction/AuctionApplication.java
@@ -4,12 +4,14 @@ import com.auction.config.AppProperties;
 import com.auction.config.ApplicationContextProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
-public class AuctionApplication {
+public class AuctionApplication extends SpringBootServletInitializer {
 
     @Bean
     public static ApplicationContextProvider contextProvider() {
@@ -20,4 +22,8 @@ public class AuctionApplication {
         SpringApplication.run(AuctionApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AuctionApplication.class);
+    }
 }
